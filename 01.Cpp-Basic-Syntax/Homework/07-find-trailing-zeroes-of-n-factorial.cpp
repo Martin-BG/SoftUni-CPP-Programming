dd2ba20ac1279f322e12f28272ec505b935888b2
MartinBG <MartinBG@abv.bg>
2017-03-17 20:49:07 +0200

    More flexible solution for trailing zeros of N! (supports bigger ithen 50000 input numbers)

--- 01.Cpp-Basic-Syntax/Homework/07-find-trailing-zeroes-of-n-factorial.cpp
+++ 01.Cpp-Basic-Syntax/Homework/07-find-trailing-zeroes-of-n-factorial.cpp
@@ -2,21 +2,30 @@
 zeros present at the end of the number N!. Examples N = 10, N! =
 3628800, answer 2; N! = 2432902008176640000, answer: 4. Make
 sure your program works for N up to 50 000*/
+//trailingZeros = num / 5 + num / 25 + num / 125 + num / 625 + num / 3125 + num / 15625;
 
 #include <iostream>
-using namespace std;
 
 int main()
 {
     int num;
 
-    cout << "Enter N: ";
+    std::cout << "Enter N: ";
 
-    cin >> num;
+    std::cin >> num;
 
-    int trailingZeroes = num / 5 + num / 25 + num / 125 + num / 625 + num / 3125 + num / 15625;
+    int trailingZeros = 0;
+    int new_zeros = 1;
+    int power_of_five = 1;
 
-    cout << num << "! has " << trailingZeroes << " trailing zero(s)." << endl;
+    for (int i = 1; new_zeros > 0; i++)
+    {
+        power_of_five *= 5;
+        new_zeros = num / power_of_five;
+        trailingZeros += new_zeros;
+    }
+
+    std::cout << num << "! has " << trailingZeros << " trailing zero(s)." << std::endl;
 
     return 0;
 }
