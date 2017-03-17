3912352d6e9ae8040b1bcfd60c1bab2402502110
MartinBG <MartinBG@abv.bg>
2017-03-17 20:47:42 +0200

    Improved solution for GCD program (better handling of 0,x and 0,0 input)

--- 01.Cpp-Basic-Syntax/Homework/06-calculate-greatest-common-divisor.cpp
+++ 01.Cpp-Basic-Syntax/Homework/06-calculate-greatest-common-divisor.cpp
@@ -1,6 +1,7 @@
 /*Write a program that calculates the greatest common divisor
 (GCD) of given two numbers. Use the Euclidean algorithm (find it
 in Internet)*/
+// http://www.calculatorsoup.com/calculators/math/gcf.php
 
 #include <iostream>
 using namespace std;
@@ -18,21 +19,34 @@ int main()
 
     if (num1 == 0 || num2 == 0)
     {
-        gcd = 0;
+        if (num1 == num2) // GCF(0,0) is undefined
+        {
+            gcd = 0;
+        }
+        else  // GCF(x,0) = x
+        {
+            gcd = (num1 != 0) ? num1 : num2;
+        }
     }
     else
     {
         gcd = findGreatesCommonDivisor(num2, num1);
-
-        if (gcd < 0)
-        {
-            gcd = -gcd;
-
-        }
     }
 
-    cout << "The greatest common divisor of "<< num1 << " and " << num2 << " is: " << gcd << endl;
+    if (gcd < 0)
+    {
+        gcd = -gcd;
 
+    }
+
+    if (gcd != 0)
+    {
+        cout << "The greatest common divisor of "<< num1 << " and " << num2 << " is: " << gcd << endl;
+    }
+    else // GCF(0,0) is undefined
+    {
+        cout << "Undefined" << endl;
+    }
     return 0;
 }
 
