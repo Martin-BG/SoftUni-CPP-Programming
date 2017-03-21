a067e3faa81154ce2b3cea6669fdbc094c6737f1
Martin Ivanov <MartinBG@abv.bg>
2017-03-21 14:39:19 +0200

    Solutions for JA1 - tasks 2 and 3

---
+++ 03.Lab-Syntax-and-Memory/Homework/JA1-Task-2-Similar-Words.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string text;
+    getline(cin, text);
+
+    string control_word;
+    cin >> control_word;
+
+    double percentage;
+    cin >> percentage;
+
+    ostringstream out_streem;
+    for (int i = 0; i< text.length(); i++)
+    {
+        if (text[i] >= 'a' && text[i] <= 'z')
+        {
+            out_streem << text[i];
+        }
+        else
+        {
+            out_streem << " ";
+        }
+    }
+
+    int similar_words = 0;
+    int control_word_size = control_word.size();
+
+    istringstream input_stream(out_streem.str());
+    string input_word;
+
+    while (input_stream >> input_word)
+    {
+        if(input_word.size() == control_word_size
+           && input_word[0] == control_word[0] )
+        {
+            int matches = 1;
+            for (int i = 1; i < control_word_size; i++)
+            {
+                if (input_word[i] == control_word[i])
+                {
+                    matches++;
+                }
+            }
+
+            if (matches * 100.0 / control_word_size >= percentage)
+            {
+                similar_words++;
+            }
+        }
+
+    }
+
+    cout << similar_words;
+
+    return 0;
+}
---
+++ 03.Lab-Syntax-and-Memory/Homework/JA1-Task-3-Fill-Matrix.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+void filler(int, int, char[][20], const int, const int, const char, const char);
+
+int main()
+{
+    int rows, columns;
+    cin >> rows >> columns;
+
+    char arr[rows][20];
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < columns; c++)
+        {
+            cin >> arr[r][c];
+        }
+    }
+
+    char fill_char;
+    cin >> fill_char;
+
+    int start_row, start_col;
+    cin >> start_row >> start_col;
+
+    char start_char = arr[start_row][start_col];
+
+    filler(start_row, start_col, arr, rows, columns, fill_char, start_char);
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < columns; c++)
+        {
+            cout << arr[r][c] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+void filler(int row, int col, char arr[][20], const int rows, const int columns, const char fill_char, const char start_char)
+{
+    arr[row][col] = fill_char;
+
+    if (row + 1 < rows && arr[row + 1][col] == start_char)
+    {
+        filler(row + 1, col, arr, rows, columns, fill_char, start_char);
+    }
+
+    if (row - 1 >= 0 && arr[row - 1][col] == start_char)
+    {
+        filler(row - 1, col, arr, rows, columns, fill_char, start_char);
+    }
+
+    if (col + 1 < columns && arr[row][col + 1] == start_char)
+    {
+        filler(row, col + 1, arr, rows, columns, fill_char, start_char);
+    }
+
+    if (col - 1 >= 0 && arr[row][col - 1] == start_char)
+    {
+        filler(row, col - 1, arr, rows, columns, fill_char, start_char);
+    }
+}
