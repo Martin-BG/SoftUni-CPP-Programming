32029db00d013275050074ceac0fe5427fe8f246
MartinBG <MartinBG@abv.bg>
2017-04-17 05:41:37 +0300

    solution for task #5 - Chess game console simulator

---
+++ 06.Full-Cpp-OOP/Homework/5-chess-game.cpp
@@ -0,0 +1,101 @@
+/* Write a program which uses the figures from Task 4 and allows two players
+ to play the game of chess on the console. Your program should do something like:
+
+intialize board;
+currentPlayer = white;
+while(no winner) {
+    print board;
+    read player input to select figure
+    //optional: show positions to which selected figure can go
+    read player input to move figure //should check for validity
+    update board; //move the figure, remove any destroyed figures, etc.
+    change currentPlayer;
+}
+print end game board and winner;
+
+Printing the board should just be printing a matrix of 8x8. Each position of the
+matrix can be a single character (you can also add the column and row designations
+if you like). You can distinguish between white and black figures by printing all
+the black figures as lowercase symbols, while printing all the white figures as
+uppercase symbols. If you want, you can play around with changing console color,
+clearing the console after each move, etc. - but that will probably make your program
+not run on every OS. So if you do that, you should submit it as a separate version and
+also submit a version that doesn't have those additional features and works on every OS.
+The structure of your program is up to you, but a good approach would be to have a
+ChessRenderer which is inherited by a ChessConsoleVisualRenderer, which prints to the
+console (so that your code easily be extended, for example, by adding a ChessConsoleTextRenderer
+which just prints stuff like "White Knight at G3"), an InputParser which reads player
+input from the console, a ChessEngine which manages all the figures, handles moves and
+figure destruction, keeps track of the game state (did the King die?/does the king have
+nowhere to move?/is the current player's king under threat, meaning that the current
+player's valid moves are restricted to defending the king?), etc., and a Chess class
+which uses all of the above in the correct order to play the game.*/
+
+#include<iostream>
+#include "5-chess-game.h"
+
+int main()
+{
+    ChessGame * game = new ChessGame();
+
+    ConsoleWriter console_writer;
+    ConsoleReader console_reader;
+
+    int turns_counter = 1;
+
+    while (!game->gameEnded())
+    {
+        std::cout << std::endl << "Turn " << turns_counter++ << std::endl << std:: endl;
+
+        console_writer.printChessBoard(game->getChessBoard());
+
+        ChessFigures * selected_figure = nullptr;
+
+        do
+        {
+            console_writer.printPlayerMoves(game->getFiguresList(), game->isWhiteTurn(), game->getChessBoard());
+            selected_figure = game->getPlayerSelectedFigure(console_reader.getUserInput());
+        }
+        while (selected_figure == nullptr );
+
+        std::string new_position;
+
+        ChessFigures * enemy_figure = nullptr;
+
+        do
+        {
+            console_writer.printFigureMoves(selected_figure, game->getChessBoard());
+
+            new_position = console_reader.getUserInput();
+
+            enemy_figure = nullptr;
+
+            if (selected_figure->isEnemyFigureAtPosition(ChessFigures::getPositionFromString(new_position), game->getChessBoard()))
+            {
+                enemy_figure = game->getFigureAtPosition(new_position);
+            }
+
+        }
+        while (!selected_figure->move(new_position, game->getChessBoard()));
+
+        if (enemy_figure != nullptr)
+        {
+            std::cout << std::endl << enemy_figure->getFigureDetailsShort() << " removed from game" << std::endl;
+
+            if (!game->deleteFigure(enemy_figure))
+            {
+                throw "Figure deletion failed";
+            }
+        }
+
+        game->updateBoard();
+
+        game->switchPlayer();
+    }
+
+    std::cout << std::endl << "Game over - " << (game->isWhiteTurn()?"Black":"White") << " victory!" << std::endl;
+
+    delete game;
+
+    return 0;
+}
---
+++ 06.Full-Cpp-OOP/Homework/5-chess-game.h
@@ -0,0 +1,248 @@
+#ifndef CHESS_GAME_H_INCLUDED
+#define CHESS_GAME_H_INCLUDED
+
+#include "4-chess-figures.h"
+#include<list>
+
+class ChessGame
+{
+public:
+    // Destructor
+    virtual ~ChessGame()
+    {
+        for (auto &it : this->m_figures)
+        {
+            delete it;
+        }
+
+        this->m_figures.clear();
+    }
+    // Constructor
+    ChessGame() :
+        m_chess_board(ChessBoard{
+        // a    b    c    d    e    f    g    h
+          'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', // 1 White
+          'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', // 2 figures
+          '0', '0', '0', '0', '0', '0', '0', '0', // 3
+          '0', '0', '0', '0', '0', '0', '0', '0', // 4
+          '0', '0', '0', '0', '0', '0', '0', '0', // 5
+          '0', '0', '0', '0', '0', '0', '0', '0', // 6
+          'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', // 7 Black
+          'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}),// 8 figures),
+        m_figures(),
+        m_game_ended(false),
+        m_white_turn(true)
+    {
+        createObjectsFromBoard();
+    }
+
+    std::list<ChessFigures*> & getFiguresList()
+    {
+        return this->m_figures;
+    }
+
+    ChessBoard & getChessBoard()
+    {
+        return this->m_chess_board;
+    }
+
+    ChessFigures * getPlayerSelectedFigure(const std::string & position_str) const
+    {
+        ChessFigures * figure = getFigureAtPosition(position_str);
+        if (figure != nullptr
+            && figure->isFigureWhite() == this->m_white_turn
+            && figure->getAvailableMoves(this->m_chess_board).size() > 0)
+        {
+            return figure;
+        }
+
+        return nullptr;
+    }
+
+    ChessFigures * getFigureAtPosition(const std::string & position_str) const
+    {
+        int position = ChessFigures::getPositionFromString(position_str);
+
+        for (auto figure : this->m_figures)
+        {
+            if (figure->getPosition() == position)
+            {
+                return figure;
+            }
+        }
+
+        return nullptr;
+    }
+
+    void updateBoard()
+    {
+        std::fill(std::begin(this->m_chess_board), std::end(this->m_chess_board), '0');
+
+        for (auto figure : this->m_figures)
+        {
+            this->m_chess_board[figure->getPosition()] = figure->getFigureSymbol();
+        }
+    }
+
+    void switchPlayer()
+    {
+        this->m_white_turn = !this->m_white_turn;
+    }
+
+    bool gameEnded() const
+    {
+        return this->m_game_ended;
+    }
+
+    bool isWhiteTurn() const
+    {
+        return this->m_white_turn;
+    }
+
+    bool deleteFigure(ChessFigures * delete_figure)
+    {
+        for (auto i = this->m_figures.begin(); i != this->m_figures.end();)
+        {
+            if ((*i) == delete_figure)
+            {
+                if (delete_figure->getType() == KING)
+                {
+                    this->m_game_ended = true;
+                }
+
+                this->m_figures.erase(i);
+                delete delete_figure;
+                return true;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+
+        return false;
+    }
+protected:
+    ChessBoard m_chess_board;
+    std::list<ChessFigures*> m_figures;
+    bool m_game_ended;
+    bool m_white_turn;
+
+private:
+    void createObjectsFromBoard()
+    {
+        for (int i = 0; i < BOARD_SIZE; i++)
+        {
+            if (this->m_chess_board[i] != '0')
+            {
+                FIGURE_COLOR color = isupper(this->m_chess_board[i])?WHITE:BLACK;
+
+                switch (tolower(this->m_chess_board[i]))
+                {
+                    case 'p' : m_figures.push_back(new Pawn(ChessFigures::getPositionName(i),color)); break;
+                    case 'n' : m_figures.push_back(new Knight(ChessFigures::getPositionName(i),color)); break;
+                    case 'b' : m_figures.push_back(new Bishop(ChessFigures::getPositionName(i),color)); break;
+                    case 'r' : m_figures.push_back(new Rook(ChessFigures::getPositionName(i),color)); break;
+                    case 'q' : m_figures.push_back(new Queen(ChessFigures::getPositionName(i),color)); break;
+                    case 'k' : m_figures.push_back(new King(ChessFigures::getPositionName(i),color)); break;
+                    default : throw "Invalid figure type"; break;
+                }
+            }
+        }
+    }
+};
+
+class ChessRender
+{
+public:
+    std::string getChessBoardString(const ChessBoard & chess_board) const
+    {
+        std::stringstream ss;
+
+        ss << "  a b c d e f g h" << std::endl;
+        for (int i = 0; i < BOARD_SIDE; i++)
+        {
+            ss << i + 1 << " ";
+
+            for (int j = 0; j < BOARD_SIDE; j++)
+            {
+                ss << chess_board[i * BOARD_SIDE + j] <<  " ";
+            }
+
+            ss << i + 1 << std::endl;
+        }
+        ss << "  a b c d e f g h" << std::endl;
+
+        return ss.str();
+    }
+
+    std::string getPlayerMovesString(const std::list<ChessFigures*> & figures, const bool white_turn, const ChessBoard & chess_board) const
+    {
+        std::stringstream ss;
+
+        for (auto figure : figures)
+        {
+            if(figure->isFigureWhite() == white_turn
+               && figure->getAvailableMoves(chess_board).size() > 0)
+            {
+                ss << figure->getFigureDetailsCompact(chess_board) << std::endl;
+            }
+        }
+
+        return ss.str();
+    }
+
+    std::string getFigureMovesString(const ChessFigures * figure, const ChessBoard & chess_board) const
+    {
+        std::stringstream ss;
+
+        ss << figure->getFigureDetailsCompact(chess_board)
+            << std::endl << "Enter new position of the figure: ";
+
+        return ss.str();
+    }
+};
+
+class ConsoleWriter : public ChessRender
+{
+public:
+    void printChessBoard(const ChessBoard & chess_board) const
+    {
+        std::cout << getChessBoardString(chess_board) << std::endl;
+    }
+
+    void printPlayerMoves(const std::list<ChessFigures*> & figures, const bool white_turn, const ChessBoard & chess_board) const
+    {
+        std::cout << "It's "
+            << (white_turn?"White":"Black")
+            << "'s turn" << std::endl << std::endl
+            << getPlayerMovesString(figures, white_turn, chess_board) << std::endl
+            << "Enter position of figure you want to move: ";
+    }
+
+    void printFigureMoves(const ChessFigures * figure, const ChessBoard & chess_board) const
+    {
+        std::cout << getFigureMovesString(figure, chess_board);
+    }
+};
+
+class ConsoleReader : public ConsoleWriter
+{
+public:
+    std::string getUserInput() const
+    {
+        std::string input;
+
+        do
+        {
+            std::cin >> input;
+        }
+        while (input.length() != 2
+                || input[0] < 'a' || input[0] > 'h'
+                || input[1] < '1' || input[1] > '8');
+
+        return input;
+    }
+};
+
+#endif // CHESS_GAME_H_INCLUDED
