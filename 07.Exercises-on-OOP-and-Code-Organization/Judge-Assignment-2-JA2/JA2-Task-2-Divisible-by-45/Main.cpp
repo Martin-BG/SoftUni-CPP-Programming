3db4934c05adf793e9cb7283fcb40b1cc2d68c18
MartinBG <MartinBG@abv.bg>
2017-04-18 02:23:34 +0300

    solution for task 2

---
+++ 07.Exercises-on-OOP-and-Code-Organization/Judge-Assignment-2-JA2/JA2-Task-2-Divisible-by-45/Main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<sstream>
+
+#include "BigInt.h"
+
+int getSumOfDigits(const std::string & digits)
+{
+    int sum = 0;
+    int str_size = digits.size();
+    for (int i = 0; i< str_size; i++)
+    {
+        sum += digits[i] - '0';
+    }
+    return sum;
+}
+
+int main()
+{
+    std::string num1, num2;
+    std::cin >> num1 >> num2;
+
+    BigInt start_num(num1);
+    BigInt end_num(num2);
+
+    int last_digit = num1[num1.size()-1] - '0';
+    int correction = 0;
+    if (last_digit != 0 && last_digit != 5)
+    {
+        if(last_digit > 5)
+        {
+            correction = 10 - last_digit;
+        }
+        else
+        {
+            correction = 5 - last_digit;
+        }
+    }
+
+    if (correction > 0)
+    {
+        start_num += BigInt(correction);
+    }
+
+
+    BigInt five(5);
+    int sum_of_digits = getSumOfDigits(num1);
+    while (sum_of_digits % 9 != 0)
+    {
+        start_num += five;
+        sum_of_digits = getSumOfDigits(start_num.getDigits());
+    }
+
+    BigInt forty_five(45);
+    while (start_num < end_num)
+    {
+        std::cout << start_num << std::endl;
+        start_num += forty_five;
+    }
+
+    return 0;
+}
